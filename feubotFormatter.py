4c5b4ad7b0ea2795f51aa80b5ba542e04d59a06a
Beansy2000 <75109494+Beansy2000@users.noreply.github.com>
2021-01-25 00:15:23 -0700

    Delete feubotFormatter.py

--- feubotFormatter.py
+++
@@ -1,6 +0,0 @@
-from discord.ext.commands import HelpFormatter
-
-class FeubotFormatter(HelpFormatter):
-    def filter_command_list(self):
-        return sorted(super().filter_command_list(), key=lambda e: str.lower(e[0]))
-
